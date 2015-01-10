55580a71d94eceb8ef56bc57f9f62a7dd9b3e7f9
rogeryee <roger.yee@hotmail.com>
2015-01-10 13:55:02 +0800

    add Gruntfile configuration

--- app/viewmodels/ko/observableSample.js
+++ app/viewmodels/ko/observableSample.js
@@ -1,18 +1,34 @@
-define(['durandal/app'],function (app) {
+define(['durandal/app','knockout'],function (app,ko) {
 
-	var myViewModel = {
-    	personName: 'Roger1234',
-    	personAge: 1234
-	};
+	function myViewModel(){
+
+		var self =  this;
+
+		// 1. Observable
+		self.personName = "Roger Yi";
+		self.personAge = 33;
+
+		// 2. Computed Variable
+		self.firstName = ko.observable('Roger');
+		self.lastName = ko.observable('Yee');
+
+		self.fullName = ko.computed(function(){
+			return self.firstName() + "." + self.lastName();
+		});
+
+
+		self.checkboxValue = ko.observable(true);
+		self.clickCheck = function(){
+			self.checkboxValue(!self.checkboxValue());
+		}
+
+		self.checkboxValue.subscribe(function(){
+
+			console.log("self.checkboxValue = " + self.checkboxValue());
+		});
+	}
 
 	// DurandalJS already do the binding on behalf of KO, so no need to do the binding explicitly.
 	//ko.applyBindings(myViewModel);
-
-	// 24*3*2 = 144
-	// (12+20+24)*3*2 = 336
-	// (16+16+20)*3*2 = 312
-	// 792m + 80z = 50*2+100*5+80 = 680z
-	// 24*10+120+60*2+50+150+120=800
-
 	return myViewModel;
 });
