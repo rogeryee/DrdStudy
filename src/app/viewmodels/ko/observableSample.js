4e43407a534bc8616d9879ceeade191ff7ee8d87
rogeryee <roger.yee@hotmail.com>
2015-04-01 13:07:04 +0800

    test

--- src/app/viewmodels/ko/observableSample.js
+++ src/app/viewmodels/ko/observableSample.js
@@ -1,4 +1,8 @@
-define(['durandal/app','knockout'],function (app,ko) {
+define(function (require) {
+
+	var app = require('durandal/app');
+	var ko = require('knockout');
+	var $ = require('jquery');
 
 	function myViewModel(){
 
@@ -23,12 +27,51 @@ define(['durandal/app','knockout'],function (app,ko) {
 		}
 
 		self.checkboxValue.subscribe(function(){
-
 			console.log("self.checkboxValue = " + self.checkboxValue());
 		});
+
+		self.getName = function(){
+			$.ajax({
+
+				type: "get",
+
+				url: "http://www.cnblogs.com/rss",
+
+				beforeSend: function(XMLHttpRequest){
+
+					console.log('beforeSend');
+				},
+				success: function(data, textStatus){
+
+					console.log('getName success');
+
+				},
+
+				complete: function(XMLHttpRequest, textStatus){
+					console.log('getName complete');
+				},
+				error: function(){
+					console.log('getName error');
+				}
+			});
+
+			// $.ajax('app/viewmodels/ko/test.json')
+			// 	.done(function(){
+
+			// 		console.log('getName done!');
+			// 	})
+			// 	.fail(function(){
+
+			// 		console.log('getName fail!');
+			// 	})
+			// 	.always(function(){
+			// 		console.log('getName always!')
+			// 	});
+
+		};
 	}
 
-	// DurandalJS already do the binding on behalf of KO, so no need to do the binding explicitly.
+	// DurandalJS already does the binding on behalf of KO, so no need to do the binding explicitly.
 	//ko.applyBindings(myViewModel);
 	return myViewModel;
 });
